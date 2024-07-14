f4266f2c20571e24c574e057903bad31494005e8
Blender555 <135155907+Blender555@users.noreply.github.com>
2024-07-14 17:52:57 +0300

    Add files via upload

---
+++ Solutions/EF/lab_1_linq/lab_1_linq/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_linq
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+    }
+}
