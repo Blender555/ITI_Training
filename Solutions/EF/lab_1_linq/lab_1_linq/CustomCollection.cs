5fb5310488ddbb3ec3912a50c5a006461c9ac8e0
Blender555 <135155907+Blender555@users.noreply.github.com>
2024-07-14 17:49:41 +0300

    Add files via upload

---
+++ Solutions/EF/lab_1_linq/lab_1_linq/CustomCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace lab_1_linq
+{
+    public class CustomCollection : IEnumerable<int>
+    {
+        private List<int> _numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new CustomEnumerator(_numbers);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return GetEnumerator();
+        }
+
+        private class CustomEnumerator : IEnumerator<int>
+        {
+            private List<int> _numbers;
+            private int _position = -1;
+
+            public CustomEnumerator(List<int> numbers)
+            {
+                _numbers = numbers;
+            }
+
+            public int Current
+            {
+                get
+                {
+                    if (_position < 0 || _position >= _numbers.Count)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return _numbers[_position];
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                _position++;
+                return (_position < _numbers.Count);
+            }
+
+            public void Reset()
+            {
+                _position = -1;
+            }
+        }
+    }
+}
---
+++ Solutions/EF/lab_1_linq/lab_1_linq/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_linq
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            Person other = (Person)obj;
+            return Name == other.Name && Age == other.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            int Hash = 11;
+            Hash = Hash * 13 + Name.GetHashCode();
+            Hash = Hash * 13 + Age.GetHashCode();
+            return Hash;
+        }
+
+        public static bool operator ==(Person p1, Person p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Person p1, Person p2) => !(p1 == p2);
+
+        public void ComparePersons(Person p)
+        {
+            Console.WriteLine($"Using Equals(): {this.Equals(p)}");
+            Console.WriteLine($"Using == operator: {this == p}");
+        }
+    }
+}
